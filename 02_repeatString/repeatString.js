20e62398ef5ce48ec22b263aa32d2ed4e203cc5e
Jacob Buck <123811017+SquidMonkey16@users.noreply.github.com>
2024-02-23 19:00:57 -0800

    Finish exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(inputString, timesRepeated)
+{
+    let finalString = "";
 
+    for (let i = 0; i < timesRepeated; ++i)
+    {
+        finalString += inputString;
+    }
+
+    if (timesRepeated < 0)
+        return "ERROR";
+
+    return finalString;
 };
 
 // Do not edit below this line
