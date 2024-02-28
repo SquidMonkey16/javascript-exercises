c983b02ed598db8b9df3321efeb652c1084544d9
Jacob Buck <123811017+SquidMonkey16@users.noreply.github.com>
2024-02-27 17:44:56 -0800

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...deleteValues) {
+    return array.filter(value => keepValue(value, deleteValues));
 };
+
+function keepValue(value, deleteValues)
+{
+    for (let delValue of deleteValues)
+    {
+        if (value === delValue)
+            return false;
+    }
+    return true;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
